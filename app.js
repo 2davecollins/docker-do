e304a886e5f3c952babf97898f7b1af3e31ebafd
2dave <2davecollins@gmail.com>
2023-03-18 16:56:30 +0000

    added stuff

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const db = require('./db');
 const sharks = require('./routes/sharks');
 
 const path = __dirname + '/views/';
-const port = 8080;
+const port = process.env.PORT || 3000;
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
@@ -13,6 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path));
 app.use('/sharks', sharks);
 
+
+
 app.listen(port, function () {
-  console.log('Example app listening on port 8080!')
+  console.log(`app listening on port $(port)!`)
 })
